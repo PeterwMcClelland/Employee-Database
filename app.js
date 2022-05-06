66c7c7e81ccde19734be70e264f27f28495a0649
Peter McClelland <peterwmcclelland@gmail.com>
2022-05-06 11:17:58 -0500

    bug fix

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ const connection = mysql.createConnection({
     user: 'root',
     password: 'Peter741!',
     database: 'employee_trackerDB'
-});
+})
 
 connection.connect(function(err){
     if (err) throw err;
     options();
-});
+})
 
 function options() {
     inquirer.prompt({
@@ -91,53 +91,51 @@ function addEmployee() {
     connection.query('SELECT * FROM role', function (err, res) {
         if (err) throw err;
         inquirer
-        .prompt([
-            { 
-                name: 'first_name',
-                type: 'input',
-                message: "Employee's first name?",
-        },
-        {
-                name: 'last_name',
-                type: 'input',
-                message: "Employee's last name?",
-        },
-        {
-                name: 'role',
-                type: 'list',
-                choices: function() {
+            .prompt([
+                {
+                    name: 'first_name',
+                    type: 'input', 
+                    message: "Employee's fist name? ",
+                },
+                {
+                    name: 'last_name',
+                    type: 'input', 
+                    message: "Employee's last name? "
+                },
+                {
+                    name: 'role', 
+                    type: 'list',
+                    choices: function() {
                     var roleArray = [];
-                    for (let i = 0; i < res.length; i++) {                      const element = array[i];
-                   roleArray.push(res[i].title);
+                    for (let i = 0; i < res.length; i++) {
+                        roleArray.push(res[i].title);
                     }
                     return roleArray;
-                },
-            message: "Employee's role?"
-        }
-        ])
-        .then(function (answer) {
-            let role_id;
-            for (let a = 0; a < res.length; a++) {
-                if (res[a].title == answer.role) {
-                    role_id = res[a].id;
-                    console.log(role_id)
+                    },
+                    message: "Employee's role? "
                 }
-            }
-            connection.query(
-                'INSERT INTO employee SET ?',
-                {
-                    first_name: answer.first_name,
-                    last_name: answer.last_name,
-                    role_id: role_id,
-                },
-                function (err) {
-                    if (err) throw err;
-                    console.log('Employee added!');
-                    options();
-                }
-            )
+                ]).then(function (answer) {
+                    let role_id;
+                    for (let a = 0; a < res.length; a++) {
+                        if (res[a].title == answer.role) {
+                            role_id = res[a].id;
+                            console.log(role_id)
+                        }                  
+                    }  
+                    connection.query(
+                    'INSERT INTO employee SET ?',
+                    {
+                        first_name: answer.first_name,
+                        last_name: answer.last_name,
+                        role_id: role_id,
+                    },
+                    function (err) {
+                        if (err) throw err;
+                        console.log('Your employee has been added!');
+                        options();
+                    })
+                })
         })
-    })
 };
 
 function addDepartment() {
@@ -215,7 +213,6 @@ function addRole() {
         })
     })
 };
-function updateRole() {};
 
 function exitApp() {
     connection.end();
